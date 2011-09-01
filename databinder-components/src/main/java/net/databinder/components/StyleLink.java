94951a0fae46194ed922fbaf41b431634933ec97
Fausto Argeni Bencosme Doñe <fbencosme@kitsd.com>
2011-09-01 18:54:29 -0400

    starting migration

--- databinder-components/src/main/java/net/databinder/components/StyleLink.java
+++ databinder-components/src/main/java/net/databinder/components/StyleLink.java
@@ -1,17 +1,17 @@
 /*
  * Databinder: a simple bridge from Wicket to Hibernate
  * Copyright (C) 2006  Nathan Hamblen nathan@technically.us
- 
+
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -19,28 +19,28 @@
 package net.databinder.components;
 
 import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.html.resources.PackagedResourceReference;
+import org.apache.wicket.resource.TextTemplateResourceReference;
 
 /**
  * Component for a stylesheet link. The stylesheet is expected to be named
- * &lt;ClassName&gt;.css for the class specified in the constructor and be located in 
+ * &lt;ClassName&gt;.css for the class specified in the constructor and be located in
  * the same package as that class.
  * @author Nathan Hamblen
  */
-public class StyleLink extends PackagedResourceReference {
-	
+public class StyleLink extends TextTemplateResourceReference {
+
 	/** Builds a StyleLinkbased on the given class. */
-	public StyleLink(String id, Class pageClass) {
+	public StyleLink(final String id, final Class pageClass) {
 		super(id, pageClass, pageClass.getSimpleName() + ".css", "href");
 	}
-	
-	protected StyleLink(String id, Class pageClass, String filename) {
+
+	protected StyleLink(final String id, final Class pageClass, final String filename) {
 		super(id, pageClass, filename, "href");
 	}
-	
+
 	/** Sets appropriate href, type, and rel values for the stylesheet. */
 	@Override
-	protected void onComponentTag(ComponentTag tag) {
+	protected void onComponentTag(final ComponentTag tag) {
 		// ensure valid css tag
 		checkComponentTag(tag, "link");
 		tag.put("type", "text/css");
--- databinder-models-hib/src/main/java/net/databinder/models/hib/SortableHibernateProvider.java
+++ databinder-models-hib/src/main/java/net/databinder/models/hib/SortableHibernateProvider.java
@@ -8,54 +8,59 @@ package net.databinder.models.hib;
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-import org.apache.wicket.extensions.markup.html.repeater.data.table.*;
-import org.apache.wicket.extensions.markup.html.repeater.data.sort.*;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortState;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortStateLocator;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
 
 /**
  * <h1>SortableHibernateProvider</h1>
  * <i>Copyright (C) 2008 The Scripps Research Institute</i>
  * <p>A HibernateProvider extension that implements ISortableDataProvider so it can be used with a DefaultDataTable or an AjaxFallbackDefaultDataTable
  * The CriteriaBuilder that handles the sorting should also implement ISortStateLocator (such as CriteriaSorter).</p>
- * 
+ *
  * @author Mark Southern (southern at scripps dot edu)
  */
 
 public class SortableHibernateProvider<T> extends HibernateProvider<T> implements ISortableDataProvider<T> {
 
-    private ISortStateLocator sortStateLocator = null;
+	private static final long serialVersionUID = 1L;
+
+	private ISortStateLocator sortStateLocator = null;
 
     private ISortState sortState;
 
-	public SortableHibernateProvider(Class<T> objectClass, CriteriaBuilder criteriaBuilder, CriteriaBuilder orderingCriteriaBuilder) {
+	public SortableHibernateProvider(final Class<T> objectClass, final CriteriaBuilder criteriaBuilder, final CriteriaBuilder orderingCriteriaBuilder) {
         super(objectClass, criteriaBuilder, orderingCriteriaBuilder);
-        if (orderingCriteriaBuilder instanceof ISortStateLocator)
-            sortStateLocator = (ISortStateLocator) orderingCriteriaBuilder;
+        if (orderingCriteriaBuilder instanceof ISortStateLocator) {
+			sortStateLocator = (ISortStateLocator) orderingCriteriaBuilder;
+		}
     }
 
-    public SortableHibernateProvider(Class<T> objectClass, OrderingCriteriaBuilder criteriaBuilder) {
+    public SortableHibernateProvider(final Class<T> objectClass, final OrderingCriteriaBuilder criteriaBuilder) {
         super(objectClass, criteriaBuilder);
-        if (criteriaBuilder instanceof ISortStateLocator)
-            sortStateLocator = (ISortStateLocator) criteriaBuilder;
+        if (criteriaBuilder instanceof ISortStateLocator) {
+			sortStateLocator = (ISortStateLocator) criteriaBuilder;
+		}
     }
 
     public ISortState getSortState() {
-        return (sortStateLocator != null) ? sortStateLocator.getSortState() : sortState;
+        return sortStateLocator != null ? sortStateLocator.getSortState() : sortState;
     }
 
-    public void setSortState(ISortState state) {
+    public void setSortState(final ISortState state) {
         if (sortStateLocator != null) {
-            sortStateLocator.setSortState(state);
+           //TODO sortStateLocator.setSortState(state);
         }
         else {
             this.sortState = state;
