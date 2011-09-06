cc36ca19878edcf56f3228a9ebf55c43e6a62de5
Fausto Argeni Bencosme Doñe <fbencosme@kitsd.com>
2011-09-06 17:54:43 -0400

    add missed changes did it  in ao sub-projects

--- databinder-components-ao/src/main/java/net/databinder/components/ao/TransactionalImageButton.java
+++ databinder-components-ao/src/main/java/net/databinder/components/ao/TransactionalImageButton.java
@@ -6,17 +6,18 @@ import net.databinder.ao.Databinder;
 import net.java.ao.EntityManager;
 import net.java.ao.Transaction;
 
-import org.apache.wicket.ResourceReference;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.markup.html.form.ImageButton;
+import org.apache.wicket.request.resource.ResourceReference;
 
 /** Button that provides a submit transaction similar to TransactionalForm. */
 public abstract class TransactionalImageButton extends ImageButton {
-	public TransactionalImageButton(String id, ResourceReference resourceReference) {
+	public TransactionalImageButton(final String id, final ResourceReference resourceReference) {
 		super(id, resourceReference);
 	}
-	
+
 	/** Called when the form is submitted, do not override if you want transactional behavior. */
+	@Override
 	public void onSubmit() {
 		try {
 			new Transaction<Object>(Databinder.getEntityManager()) {
@@ -26,13 +27,13 @@ public abstract class TransactionalImageButton extends ImageButton {
 					return null;
 				}
 			}.execute();
-		} catch (SQLException e) {
+		} catch (final SQLException e) {
 			throw new WicketRuntimeException(e);
 		}
 		afterSubmit();
 	}
-	
-	/** 
+
+	/**
 	 * Called inside onSubmit's database transaction.
 	 * @param entityManager associated with button, provided for convenience
 	 */
